21930404165cd2d106b55847eb98ef86d21526c9
Leif Arne Storset <leifarne@storset.net>
2017-07-11 22:33:08 +0000

    Rewind posts (avoid blank pages)
    Also refactor a bit. (I thought I needed to do that to get AddThis to
    work, but it turned out to be unnecessary.)

--- canonical-http.php
+++ canonical-http.php
@@ -7,20 +7,34 @@
 
 $modify_before_date = '2017-07-10';
 
-function canon_modify_canonical_url($canonical_url) {
+function canon_should_modify() {
     global $modify_before_date;
 
-	if(is_single() || is_page() ){
-        while (have_posts()) {
-            the_post();
+    try {
+        if ((is_single() || is_page()) && have_posts()) {
+            while (have_posts()) {
+                the_post();
 
-            $date = get_the_date('Y-m-d');
+                $date = get_the_date('Y-m-d');
 
-            if ($date < $modify_before_date) {
-                // return $canonical_url ."#$modify_before_date";
-                return str_replace('https', 'http', $canonical_url);
+                if ($date < $modify_before_date) {
+                    return true;
+                }
             }
         }
+        return false;
+    } finally {
+        rewind_posts();
+    }
+}
+
+function canon_modify_url($original_url) {
+    return str_replace('https', 'http', $original_url);
+}
+
+function canon_modify_canonical_url($canonical_url) {
+    if (canon_should_modify()) {
+        return canon_modify_url($canonical_url);
     }
     return $canonical_url;
 }
