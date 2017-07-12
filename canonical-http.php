ecacc6b064a783c56eb8d141c469548e5943d38e
Leif Arne Storset <leifarne@storset.net>
2017-07-12 01:24:45 +0000

    Use correct permalink filters
    the_permalink is only for display.

--- canonical-http.php
+++ canonical-http.php
@@ -30,8 +30,9 @@ function canon_modify_canonical_url($canonical_url) {
 
 add_filter('get_canonical_url', 'canon_modify_canonical_url');
 
-// AddThis *should* use this (but uses get_permalink currently)
-add_filter('the_permalink', 'canon_modify_canonical_url');
+// AddThis uses the permalink
+add_filter('post_link', 'canon_modify_canonical_url');
+add_filter('page_link', 'canon_modify_canonical_url');
 
 // Yoast SEO has its own canonical URL
 add_filter('wpseo_canonical', 'canon_modify_canonical_url');
